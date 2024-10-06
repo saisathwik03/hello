1e671577ea7ed9a6de30c3164d1b13ee315a93a9
saisathwik03 <saisathwik173@gmail.com>
2024-10-06 22:53:17 +0530

    Sum of 2 numbers program

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,8 @@
+def sum_of_two_numbers(a,b):
+    result = a+b
+    return result
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = sum_of_two_numbers(a,b)
+print("Sum of a & b is : ", c)
